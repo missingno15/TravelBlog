7da04af7c7da10412aee4ba118482926fbb3f888
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-14 21:36:36 -0400

    Added presence validation for location_name on LocationPin

--- app/models/location_pin.rb
+++ app/models/location_pin.rb
@@ -4,8 +4,9 @@ class LocationPin < ActiveRecord::Base
   has_many :photos, dependent: :destroy
   has_many :posts, dependent: :destroy
 
-  validates :map, :presence => true
-  validates :trip, :presence => true
+  validates :map, presence: true
+  validates :trip, presence: true
   validates :longitude, numericality: true
   validates :latitude, numericality: true
+  validates :location_name, presence: true
 end
